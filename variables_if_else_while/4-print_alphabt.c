a97bbfae582c899ec8d3b5a985ac3b47dd7058e5
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2025-12-30 22:24:46 +0000

    Add 4-print_alphabt

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet except q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}

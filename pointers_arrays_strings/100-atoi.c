ec20f192ac8dc3ea59f89689aa5a1ce5d231cffc
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2025-12-31 11:37:57 +0000

    Fix _atoi overflow handling

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,20 +1,17 @@
 #include "main.h"
-#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
  * @s: string to convert
  *
- * Return: integer value
+ * Return: the converted integer
  */
 int _atoi(char *s)
 {
 	int i = 0;
 	int sign = 1;
 	unsigned int num = 0;
-	unsigned int limit;
 
-	/* find first digit, track signs before digits */
 	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
@@ -22,32 +19,18 @@ int _atoi(char *s)
 		i++;
 	}
 
-	/* limit depends on sign: INT_MAX or (INT_MAX + 1) for INT_MIN */
-	if (sign == -1)
-		limit = (unsigned int)INT_MAX + 1U;
-	else
-		limit = (unsigned int)INT_MAX;
-
-	/* build number without overflowing */
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		unsigned int digit = (unsigned int)(s[i] - '0');
-
-		if (num > (limit - digit) / 10)
+		if (num > (unsigned int)2147483647 / 10 ||
+		    (num == (unsigned int)2147483647 / 10 && (s[i] - '0') > 7))
 		{
-			num = limit;
-			break;
+			if (sign == -1)
+				return (-2147483648);
+			return (2147483647);
 		}
-		num = num * 10 + digit;
+		num = num * 10 + (s[i] - '0');
 		i++;
 	}
 
-	if (sign == -1)
-	{
-		if (num == (unsigned int)INT_MAX + 1U)
-			return (INT_MIN);
-		return (-(int)num);
-	}
-
-	return ((int)num);
+	return (sign * num);
 }

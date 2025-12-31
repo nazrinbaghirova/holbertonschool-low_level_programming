228d4ae85b844f33740b7112f0aadc500ca383b2
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2025-12-31 00:06:31 +0000

    Add 1-alphabet

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
-* _putchar - writes the character c to stdout
+ * _putchar - writes the character c to stdout
 * @c: The character to print
 *
 * Return: 1 on success, -1 on error

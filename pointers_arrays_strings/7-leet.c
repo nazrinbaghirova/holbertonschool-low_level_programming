b599a6cc2a58e07814cc3ee20e8feeae6447d250
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2025-12-31 19:00:36 +0000

    Add leet encoder

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string to encode
+ *
+ * Return: pointer to s
+ */
+char *leet(char *s)
+{
+	int i = 0, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (letters[j] != '\0')
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}

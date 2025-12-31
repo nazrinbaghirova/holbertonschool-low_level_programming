84210730acd89a1ae020ad50431394457a32d29c
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2025-12-31 20:30:10 +0000

    Add _strspn implementation

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-int _putchar(char c);
+unsigned int _strspn(char *s, char *accept);
 
 #endif
-
-

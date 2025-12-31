85fa2c1d3b7211025e63a90857fea7739364663c
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2025-12-31 18:34:06 +0000

    Add reverse_array function

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-char *_strncat(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
 
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
 #endif

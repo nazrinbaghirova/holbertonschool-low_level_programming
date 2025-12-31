a0c6b6020a1367b89330c413b1515e2fb09e900c
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2025-12-31 00:24:39 +0000

    Add _islower function

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
 *
 * Return: 1 on success, -1 on error
 */

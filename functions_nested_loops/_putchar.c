6210ce84d701da076a39885098950b0be0396f07
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2025-12-31 00:31:20 +0000

    Add _isalpha function

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -3,10 +3,10 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
-*
-* Return: 1 on success, -1 on error
-*/
+ *
+ * Return: 1 on success, -1 on error
+ */
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }

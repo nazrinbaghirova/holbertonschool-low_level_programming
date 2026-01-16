946d7107f9970132a2f573661f161e43deb88a1d
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-01-16 06:11:06 +0000

    Fix print_all to satisfy if/loop restrictions

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,24 +1,60 @@
-#include <stdio.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
- * print_all - prints anything, followed by a new line
+ * print_all - Prints anything based on a format string
  * @format: list of types of arguments passed to the function
  *
- * Return: nothing
+ * Return: void
  */
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	unsigned int i = 0;
-	char *sep = "";
-	char *s;
+	unsigned int i;
+	char *sep;
+	char *str;
+
+	if (format == NULL)
+	{
+		printf("\n");
+		return;
+	}
 
 	va_start(ap, format);
 
-	while (format && format[i])
+	i = 0;
+	sep = "";
+
+	while (format[i] != '\0')
 	{
 		switch (format[i])
 		{
-		case 'c':
+			case 'c':
+				printf("%s%c", sep, va_arg(ap, int));
+				sep = ", ";
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(ap, int));
+				sep = ", ";
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(ap, double));
+				sep = ", ";
+				break;
+			case 's':
+				str = va_arg(ap, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+				sep = ", ";
+				break;
+			default:
+				break;
+		}
+		i++;
+	}
 
+	va_end(ap);
+	printf("\n");
+}

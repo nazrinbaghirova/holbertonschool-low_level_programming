e1e117ada4c563ce32daad45cf8734262e7b71b5
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-01-16 05:24:58 +0000

    Add README and fix Betty issues for variadic_functions

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,7 +2,7 @@
 
 /**
  * sum_them_all - returns the sum of all its parameters
- * @n: number of arguments
+ * @n: number of arguments passed to the function
  *
  * Return: sum of parameters, or 0 if n is 0
  */

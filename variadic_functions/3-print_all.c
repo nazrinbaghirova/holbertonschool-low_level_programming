c5e0410f1a35d7bc50be5192957177c49b7c98fa
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-01-16 05:57:53 +0000

    Fix print_all to satisfy checker and Betty

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,9 +1,37 @@
-
 #include <stdio.h>
 #include "variadic_functions.h"
 
 /**
- * print_all - prints anything followed by a new line
+ * print_one - prints one argument based on format character
+ * @c: format character
+ * @ap: argument list
+ * @sep: separator string
+ *
+ * Return: new separator string
+ */
+static const char *print_one(char c, va_list ap, const char *sep)
+{
+	char *s;
+
+	if (c == 'c')
+		return (printf("%s%c", sep, (char)va_arg(ap, int)), ", ");
+	if (c == 'i')
+		return (printf("%s%d", sep, va_arg(ap, int)), ", ");
+	if (c == 'f')
+		return (printf("%s%f", sep, va_arg(ap, double)), ", ");
+	if (c == 's')
+	{
+		s = va_arg(ap, char *);
+		if (s == NULL)
+			s = "(nil)";
+		printf("%s%s", sep, s);
+		return (", ");
+	}
+	return (sep);
+}
+
+/**
+ * print_all - prints anything, followed by a new line
  * @format: list of types of arguments passed to the function
  *
  * Return: nothing
@@ -12,7 +40,7 @@ void print_all(const char * const format, ...)
 {
 	va_list ap;
 	unsigned int i = 0;
-	char *sep = "";
+	const char *sep = "";
 
 	if (format == NULL)
 	{
@@ -21,8 +49,12 @@ void print_all(const char * const format, ...)
 	}
 
 	va_start(ap, format);
-	while (format[i])
-		sep = print_by_format(format[i++], ap, sep);
+	while (format[i] != '\0')
+	{
+		sep = print_one(format[i], ap, sep);
+		i++;
+	}
 	va_end(ap);
+
 	printf("\n");
 }

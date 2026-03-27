5b3d75ddfe4a43594ebd46dd49c1dc39e2f50a9d
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-03-27 16:21:34 +0000

    Add hash_table_delete function

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -30,6 +30,7 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_delete(hash_table_t *ht);
 void hash_table_print(const hash_table_t *ht);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 hash_node_t *create_node(const char *key, const char *value);

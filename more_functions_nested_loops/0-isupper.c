69d235d81599072d7ba6587b41908f56247f65b8
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-08-15 14:29:12 +0400

    Add _isupper function

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -4,11 +4,12 @@
  * _isupper - checks if a character is uppercase
  * @c: character to check
  *
- * Return: 1 if uppercase, 0 otherwise
+ * Return: 1 if c is uppercase, 0 otherwise.
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
+
 	return (0);
 }

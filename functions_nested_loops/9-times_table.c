7020ef21afcaa9fb6db3adf551a21030b2eeb6ea
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-08-15 14:18:11 +0400

    Fix times table prototype

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -18,15 +18,18 @@ void times_table(void)
 			product = row * column;
 
 			if (column != 0)
-				_putchar(','), _putchar(' ');
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 
 			if (product < 10)
 				_putchar(' ');
 
+			_putchar((product % 10) + '0');
+
 			if (product >= 10)
 				_putchar((product / 10) + '0');
-
-			_putchar((product % 10) + '0');
 		}
 		_putchar('\n');
 	}

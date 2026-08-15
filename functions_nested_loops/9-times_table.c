e7871cb8aace077da9d19d284dfc0d0f192d29e3
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-08-15 14:22:38 +0400

    Fix times table formatting

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -23,13 +23,13 @@ void times_table(void)
 				_putchar(' ');
 			}
 
-			if (product < 10)
+			if (column != 0 && product < 10)
 				_putchar(' ');
-
-			_putchar((product % 10) + '0');
 
 			if (product >= 10)
 				_putchar((product / 10) + '0');
+
+			_putchar((product % 10) + '0');
 		}
 		_putchar('\n');
 	}

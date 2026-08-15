9017663e525960bb85c1a92f8854095e3783bd1a
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-08-15 14:02:21 +0400

    Add print_sign function

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * print_sign - prints the sign of a number
  * @n: number to check
  *
- * Return: 1 if positive, 0 if zero, -1 if negative
+ * Return: 1 if n is positive, 0 if n is zero, -1 if n is negative.
  */
 int print_sign(int n)
 {
